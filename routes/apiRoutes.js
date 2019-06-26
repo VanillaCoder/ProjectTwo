4c5a293990ed0165d5aa1ef3b1a4cb3cfadc4798
LiamH <liamhearts@gmail.com>
2019-06-26 13:23:38 -0700

    very basic handlebar/api testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,33 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/categories", function (req, res) {
+   db.Category.findAll({}).then(function(categorydb) {
+     res.json(categorydb);
+   });
+  });
+  app.get("/api/create", function(req, res) {
+    db.Category.create({
+      catID: "coding"
+    });
+    db.Category.create({
+      catID: "politics"
+    });
+    db.Category.create({
+      catID: "general"
     });
   });
-
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
