537857d39fd44cb36a3ecf12cdce4b2dfd08e150
LiamH <liamhearts@gmail.com>
2019-06-27 19:18:05 -0700

    config stuff

--- models/category.js
+++ models/category.js
@@ -1,8 +1,9 @@
 module.exports = function (sequelize, DataTypes) {
     var Category = sequelize.define("Category", {
       // Giving the Author model a name of type STRING
-      catID: DataTypes.STRING
+      URL: DataTypes.STRING,
+      title: DataTypes.STRING,
+      text: DataTypes.STRING
     });
     return Category;
 };
-  
