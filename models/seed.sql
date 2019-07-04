be2177e049293dd8d9054bd3cf6bb53cf59ca61d
eddsonblanco <eddsonrbr@gmail.com>
2019-07-03 23:41:20 -0700

    styles footer and home hbs

--- models/seed.sql
+++ models/seed.sql
@@ -37,9 +37,9 @@ INSERT INTO `cldb`.`Categories`
 `updatedAt`)
 VALUES
 (
-'https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=490&q=80',
+'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=490&q=80',
 'Coding',
-'Best Tech',
+'Best Tech.',
 '2016-05-04 15:59:21 ',
 '2016-05-04 15:59:21 ');
 INSERT INTO `cldb`.`Categories`
